f7dae4459c7124a8d4f8c245ef445d8544760acb
Nam <nduongbao@ploggmedia.com>
2023-08-12 16:05:31 +0700

    chore: refactor

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,13 +1,13 @@
 import { useContext, useEffect } from 'react';
 import dynamic from 'next/dynamic';
-import { styled } from 'styled-components';
 
 import { LoadingContext } from '@/contexts/LoadingContext';
-import LoadingChildOut from '@/components/common/LoadingChildOut';
 import Cursor from '@/components/common/Cursor';
 import RouteLoading from '@/components/common/RouteLoading';
 import ExpandCircle from '@/components/common/ExpandCircle';
 import BackgroundLine from '@/components/common/BackgroundLine';
+
+import { Container } from './styles';
 
 const Header = dynamic(() => import('@/components/common/Header'), {
   ssr: false,
@@ -15,13 +15,6 @@ const Header = dynamic(() => import('@/components/common/Header'), {
 const IntroLoading = dynamic(() => import('@/components/common/IntroLoading'), {
   ssr: false,
 });
-
-const Container = styled.div`
-  position: relative;
-  background-color: #101010;
-  width: 100%;
-  height: 100%;
-`;
 
 function Layout({ children }: { children: React.ReactNode }) {
   const { introLoading } = useContext(LoadingContext);
---
+++ src/layouts/styles.tsx
@@ -0,0 +1,8 @@
+import { styled } from 'styled-components';
+
+export const Container = styled.div`
+  position: relative;
+  background-color: #101010;
+  width: 100%;
+  height: 100%;
+`;
